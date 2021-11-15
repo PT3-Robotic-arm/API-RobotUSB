9ae8386a5def9c773f6772da39e7766e51d41528
Zalaxx <leo.jaillet@protonmail.com>
2021-11-15 22:04:52 +0100

    allow any origin

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 3000;
 
 const api = require("./routes/api");
 
+app.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", "*");
+    next()
+})
+
 app.use("/api", api);
 
 app.listen(3000, () => {
